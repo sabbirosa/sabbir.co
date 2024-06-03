50a452b36acdc3be09ba33e41a9cbe9326b84a08
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2024-06-03 20:49:37 +0600

    Fix: About me text edited

--- data/index.ts
+++ data/index.ts
@@ -49,8 +49,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
+    title: "Currently building a Club Management System",
+    description: "With the power of NextJs",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
